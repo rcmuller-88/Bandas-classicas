6d701331eefe5096a916326bc653fc50c4bf0401
rcmuller-88 <mullerinvestimentos88@gmail.com>
2024-09-06 19:07:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    
+        {
+          titulo: "Led Zeppelin",
+          descricao: "Pioneiros do hard rock, o Led Zeppelin combinava elementos de blues, folk e rock psicodélico, criando um som poderoso e influente",
+          link: "https://pt.wikipedia.org/wiki/Led_Zeppelin",
+          tags: "John Bonham A Força Bruta na Bateria John Paul Jones O Multi instrumentista Robert Plant A Voz Poderosa Jimmy Page O Mestre da Guitarra"
+},
+    
+        {
+          titulo: "The Rolling Stones",
+          descricao: "Uma das bandas de rock mais influentes de todos os tempos, os Rolling Stones são conhecidos por suas letras provocativas, performances energéticas e longa carreira.",
+          link: "https://pt.wikipedia.org/wiki/The_Rolling_Stones",
+          tags: "Mick Jagger Vocalista Keith Richards Guitarrista Ronnie Wood Guitarrista Charlie Watts Bateria falecido em 2021"
+        },
+
+        {
+          titulo: "Pink Floyd",
+          descricao: "Pioneiros do rock psicodélico e progressivo, o Pink Floyd é famoso por seus álbuns conceituais e experimentações sonoras.",
+          link: "https://pt.wikipedia.org/wiki/Pink_Floyd",
+          tags: "Syd Barrett Nick Mason Roger Waters Richard Wright"
+        }
+      ];
+       
+
+
+
+  
